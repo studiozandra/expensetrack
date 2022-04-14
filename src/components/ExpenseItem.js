16b1bd4f6c519b3b583a9c7e32787d8d1a0f95a8
zan <zan@studiozandra.com>
2022-04-13 23:00:45 -0400

    add ExpenseItem css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,10 @@
 function ExpenseItem() {
-    return <h2> Expense item</h2>
+    return (
+        <div>
+            <div>Date</div>
+            <h2> Title</h2>
+            <div>Amount</div>
+        </div>)
 }
 
 export default ExpenseItem
