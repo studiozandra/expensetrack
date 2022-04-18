8de85ff0905ae174c5837943feff81443fc9e960
zan <zan@studiozandra.com>
2022-04-18 11:28:44 -0400

    make component a bit more reusable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 4, 16);
+    const expenseTitle = 'Car insurance';
+    const expenseAmount = 28.0;
+
     return (
         <div className="expense-item">
-            <div>April 13, 2022</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Poke burrito</h2>
-                <div className="expense-item__price">$29.98</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>)
 }
