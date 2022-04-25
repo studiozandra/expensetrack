e2a6ed72adeb95b0d267ddee6ebeee2fcafcbea1
zan <zan@studiozandra.com>
2022-04-24 20:28:01 -0400

    add format date function

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,12 +7,24 @@ function ExpenseItem() {
 
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{formatDate(expenseDate)}</div>
             <div className="expense-item__description">
                 <h2>{expenseTitle}</h2>
                 <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>)
 }
+
+// format the date object
+function formatDate(date) {
+    var cdate = new Date(date);
+    var options = {
+        year: "numeric",
+        month: "short",
+        day: "numeric"
+    };
+    date = cdate.toLocaleDateString("en-us", options);
+    return date;
+};
 
 export default ExpenseItem;
