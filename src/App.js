028d289ecdbc6cb82dc6b728a7b44651076c24aa
zan <zan@studiozandra.com>
2022-04-24 20:47:35 -0400

    change title, add expense obj

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import ExpenseItem from "./components/ExpenseItem";
 
+const expenses = [
+  {
+    id: 'e1',
+    title: 'Star Trek-related expenditures',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'gifts',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'chocolate purchases',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>Expense Tracker</h2>
       <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
