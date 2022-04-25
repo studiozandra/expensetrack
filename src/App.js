a376d9ff37e72e0e8b3623713a5a9f1b5fde75e7
zan <zan@studiozandra.com>
2022-04-24 20:58:15 -0400

    add and pass props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const expenses = [
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', title: 'New bike', amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: 'e3',
     title: 'gifts',
@@ -26,9 +26,10 @@ function App() {
   return (
     <div>
       <h2>Expense Tracker</h2>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2022, 4, 16);
-    const expenseTitle = 'Car insurance';
-    const expenseAmount = 28.0;
+function ExpenseItem(props) {
 
     return (
         <div className="expense-item">
-            <div>{formatDate(expenseDate)}</div>
+            <div>{formatDate(props.date)}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>)
 }
